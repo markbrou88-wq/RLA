27ece3418c7a2334a1fce13a8f3f43935b4573dc
markbrou88-wq <mark.brou88@gmail.com>
2025-10-23 18:36:15 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY
+const url = "https://tmodvqenwgxojmjiyknr.supabase.co"
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtb2R2cWVud2d4b2ptaml5a25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDc1NjgsImV4cCI6MjA3NjgyMzU2OH0.Fy3iVihLEPSP58C3Fz5tw3GcJ4iidAXkz_3e8iqFddQ"
 
 export const supabase = createClient(url, key, {
   auth: { persistSession: true, autoRefreshToken: true },
