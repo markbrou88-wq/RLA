3dbab74f22bf077350e515b2d0c1c3cfcee585be
markbrou88-wq <mark.brou88@gmail.com>
2025-10-26 21:58:58 -0400

    Create LanguageToggle.jsx

---
+++ src/components/LanguageToggle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useI18n } from "../i18n";
+
+export default function LanguageToggle() {
+  const { lang, setLang } = useI18n();
+  const next = lang === "en" ? "fr" : "en";
+  return (
+    <button className="btn secondary" onClick={() => setLang(next)} title="Language">
+      {lang === "en" ? "Français" : "English"}
+    </button>
+  );
+}
