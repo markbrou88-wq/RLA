a0719e1b16dfcfbe6383e81e5a7b608bb0157f30
markbrou88-wq <mark.brou88@gmail.com>
2025-12-13 22:41:35 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { SeasonProvider } from './SeasonContext.jsx'
+import { SeasonProvider } from './contexts/SeasonContext'
 import './v2-styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
