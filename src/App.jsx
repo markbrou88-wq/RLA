a66b2812edb88161cfe5198de9d5b6bd81cf8918
markbrou88-wq <mark.brou88@gmail.com>
2026-01-25 13:25:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import GamesPage from "./pages/GamesPage";
 import StatsPage from "./pages/StatsPage";
 import SummaryPage from "./pages/SummaryPage";
 import LivePage from "./pages/LivePage";
-import LiveQuickPage from "./pages/LiveQuickPage";
 import RosterPage from "./pages/RosterPage";
 import TeamPage from "./pages/TeamPage";
 import PlayerPage from "./pages/PlayerPage";
@@ -172,7 +171,7 @@ function AppInner() {
           <Route path="/stats" element={<StatsPage />} />
           <Route path="/summary/:slug" element={<SummaryPage />} />
           <Route path="/live/:slug" element={<LivePage />} />
-          <Route path="/live-quick/:slug" element={<LiveQuickPage />} />
+          
 
          <Route path="/games/:slug/roster" element={<RosterPage />} />
 
