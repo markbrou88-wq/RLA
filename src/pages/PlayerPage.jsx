9fd1e6d2c2f5a00bc8ebea28cab694638b2fd1cc
markbrou88-wq <mark.brou88@gmail.com>
2026-01-31 16:25:12 -0500

    Update PlayerPage.jsx

--- src/pages/PlayerPage.jsx
+++ src/pages/PlayerPage.jsx
@@ -105,7 +105,7 @@ if (isGoalie) {
     goalieCareerTotals.gaa =
   goalieCareerTotals.toi > 0
     ? Math.round(
-        (goalieCareerTotals.ga / goalieCareerTotals.toi) * 60 * 100
+        (goalieCareerTotals.ga / goalieCareerTotals.toi) * 30 * 100
       ) / 100
     : null;
   }
@@ -152,7 +152,7 @@ if (isGoalie) {
   ...r,
   gaa:
     r.toi_seconds > 0
-      ? Math.round((r.ga / r.toi_seconds) * 60 * 100) / 100
+      ? Math.round((r.ga / r.toi_seconds) * 30 * 100) / 100
       : null,
 }));
 
